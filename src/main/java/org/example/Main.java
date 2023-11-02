75b48fe75554cf858342f484d234f2aa10fbbb09
hcs <hannahemcs@gmail.com>
2023-11-02 15:50:13 +0100

    Created reports and tidied up everything in main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,6 +18,9 @@ public class Main {
             return displayName;
         }
     }
+    public interface Report {
+        String generateReport(Set<Employee> employees);
+    }
 
     public static void searchDept(String searchTerm, HashSet<Employee> employees) {
         List<Employee> searchResults = new ArrayList<>();
@@ -29,7 +32,7 @@ public class Main {
         System.out.println("Results: " + searchResults);
     }
 
-    public static void departmentReport(Set<Employee> employees) {
+    public static void generateDepartmentReport(Set<Employee> employees) {
         Map<String, Integer> deptCount = new HashMap<>();
         for (Employee e : employees) {
             String dept = e.getDepartment();
@@ -40,58 +43,65 @@ public class Main {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
+    public static void generateEmployeeReport(Set<Employee> employees) {
+        System.out.println("All employees:");
+        for (Employee employee : employees) {
+            System.out.println(employee.toString());;
+        }
+    }
+
+    public static void generateHierarchyReport(EmployeeNode ceo) {
+        System.out.println("Hierarchy Report:");
+        printHierarchy(ceo, 0);
+    }
+
+    public static void printHierarchy(EmployeeNode node, int level) {
+        if (node != null) {
+            Employee employee = node.getEmployee();
+            System.out.println("  ".repeat(level) + employee.getName() + " - " + employee.getDepartment());
+
+            for (EmployeeNode subordinate : node.getSubordinates()) {
+                printHierarchy(subordinate, level + 1);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         HashSet<Employee> employees = new HashSet<>();
-        employees.add(new Employee("Ann", 001, "Finance", "Accountant", 004, "2018-10-03", 75000));
-        employees.add(new Employee("Bob", 002, "HR", "Recruiter", 432, "2020-04-06", 60000));
-        employees.add(new Employee("Sam", 003, "Sales", "Sales rep", 328, "2023-08-04", 45000));
-        System.out.println(employees);
+        Employee employee001 = new Employee("Ann", 001, "Finance", "Accountant", 004, "2018-10-03", 75000);
+        Employee employee002 = new Employee("Bob", 002, "HR", "Recruiter", 432, "2020-04-06", 60000);
+        Employee employee003 = new Employee("Sam", 003, "Sales", "Sales rep", 328, "2023-08-04", 45000);
+        Employee ceoEmployee = new Employee("Marie", 1, "Leadership", "CEO", 1, "2015-07-01", 400000);
+        Employee ctoEmployee = new Employee("Liz", 4, "Finance", "CTO", 1, "2017-05-03", 100000);
+        Employee hrHeadEmployee = new Employee("Tom", 432, "HR", "Head of HR", 1, "2022-09-08", 90000);
+        Employee salesDirectorEmployee = new Employee("Frank", 328, "Sales", "Sales Director", 1, "2016-03-14", 150000);
+
+        employees.add(employee001);
+        employees.add(employee002);
+        employees.add(employee003);
+        employees.add(ceoEmployee);
+        employees.add(ctoEmployee);
+        employees.add(hrHeadEmployee);
+        employees.add(salesDirectorEmployee);
 
         HashMap<Integer, Employee> employeeMap = new HashMap<>();
         for (Employee employee : employees) {
             employeeMap.put(employee.getId(), employee);
         }
 
-        System.out.println(employeeMap);
-
-        // CRUD operations:
-        Employee newEmployee = new Employee("Liz", 004, "Finance", "CTO", 1, "2017-05-03", 100000);
-        employees.add(newEmployee);
-        // Create
-        employeeMap.put(004, newEmployee);
-        System.out.println(employeeMap);
-        // Retrieve
-        System.out.println(employeeMap.get(004));
-        // Update
-        newEmployee.setName("Elizabeth");
-        System.out.println(employeeMap);
-        // Delete
-        employeeMap.remove(004);
-        System.out.println(employeeMap);
-
-        // Search by department
-        searchDept("Finance", employees);
-
-        //dept report
-        departmentReport(employees);
-
-        // organisational tree
-        EmployeeNode ceo = new EmployeeNode(new Employee("Marie", 1, "Leadership", "CEO", 1, "2015-07-01", 400000));
-        EmployeeNode manager1 = new EmployeeNode(new Employee("Liz", 4, "Finance", "CTO", 1, "2017-05-03", 100000));
-        EmployeeNode manager2 = new EmployeeNode(new Employee("Tom", 432, "HR", "Head of HR", 1,  "2022-09-08", 90000));
-        EmployeeNode manager3 = new EmployeeNode(new Employee("Frank", 328, "Sales", "Sales Director", 1, "2016-03-14", 150000));
-        EmployeeNode employee001 = new EmployeeNode(employeeMap.get(1));
-        EmployeeNode employee002 = new EmployeeNode(employeeMap.get(2));
-        EmployeeNode employee003 = new EmployeeNode(employeeMap.get(3));
-
-        // Build the organization hierarchy
+        EmployeeNode ceo = new EmployeeNode(ceoEmployee);
+        EmployeeNode manager1 = new EmployeeNode(ctoEmployee);
+        EmployeeNode manager2 = new EmployeeNode(hrHeadEmployee);
+        EmployeeNode manager3 = new EmployeeNode(salesDirectorEmployee);
         ceo.addSubordinate(manager1);
         ceo.addSubordinate(manager2);
         ceo.addSubordinate(manager3);
-        manager1.addSubordinate(employee001);
-        manager2.addSubordinate(employee002);
-        manager3.addSubordinate(employee003);
-    }
+        manager1.addSubordinate(new EmployeeNode(employee001));
+        manager2.addSubordinate(new EmployeeNode(employee002));
+        manager3.addSubordinate(new EmployeeNode(employee003));
 
+        generateDepartmentReport(employees);
+        generateEmployeeReport(employees);
+        generateHierarchyReport(ceo);
+    }
 }
